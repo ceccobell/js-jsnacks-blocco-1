3407eb3723d72872d99f7a43d7eeef01885fa59d
ceccobell <francescobell2001@gmail.com>
2024-07-01 15:23:33 +0200

    [UPD] bonus completed

--- bonus/script.js
+++ bonus/script.js
@@ -3,4 +3,8 @@ let somma = 0
 for (i = 1; i <= 10; i++) {
     let num = prompt("inserisci un numero")
     console.log(i, " numero inserito", num)
+
+    somma += parseInt(num)
 }
+
+console.log("La somma è ", somma)
